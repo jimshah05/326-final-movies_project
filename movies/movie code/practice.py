1c1b0ec31c8ce2088bc583ec3633ff89a9174860
Jamshaid Shah <jam.shah222@gmail.com>
2026-04-26 15:51:26 -0400

    testing repository changes

---
+++ movies/movie code/practice.py
@@ -0,0 +1,14 @@
+class book:
+
+    def __init__(self , title , genre):
+
+        self.title = title 
+        self.genre = genre
+
+class Library:
+
+    def __init__(self , name):
+
+        self.name = name 
+        self.books = [] 
+        
